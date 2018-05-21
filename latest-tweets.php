716806f0d96151a61f6884daac3486d692ff1ee6
Karol Orzeł <karol@rockettheme.com>
2018-05-21 16:12:38 +0200

    Initial examples styling

--- latest-tweets.php
+++ latest-tweets.php
@@ -10,15 +10,16 @@ class LatestTweetsPlugin extends Plugin
         return [
             'onPluginsInitialized' => ['onPluginsInitialized', 0],
             'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
         ];
     }
     public function onPluginsInitialized()
-    {   
+    {
         require_once __DIR__ . '/tweets.php';
         $this->grav['twig']->twig_vars['latesttweets'] = display_latest_tweets(
-            $this->config->get('plugins.latest-tweets.twitter_id'), 
-            $this->config->get('plugins.latest-tweets.consumerkey'), 
-            $this->config->get('plugins.latest-tweets.consumersecret'), 
+            $this->config->get('plugins.latest-tweets.twitter_id'),
+            $this->config->get('plugins.latest-tweets.consumerkey'),
+            $this->config->get('plugins.latest-tweets.consumersecret'),
             $this->config->get('plugins.latest-tweets.accesstoken'),
             $this->config->get('plugins.latest-tweets.accesstokensecret'),
             $this->config->get('plugins.latest-tweets.tweets_to_display'),
@@ -29,5 +30,14 @@ class LatestTweetsPlugin extends Plugin
     public function onTwigTemplatePaths()
     {
         $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+    }
+
+    public function onTwigSiteVariables()
+    {
+
+        if ($this->config->get('plugins.latest-tweets.built_in_css')) {
+            $this->grav['assets']
+                ->add('plugin://latest-tweets/assets/css/latest-tweets.css');
+        }
     }
 }
